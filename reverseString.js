5e350aacd4116e803f6f2b02da5f009fa7d1bcf0
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-11-19 16:28:43 +0200

    Update reverseString.js

--- reverseString.js
+++ reverseString.js
@@ -1,3 +1,4 @@
+// my solution
 const reverse = (str) => {
   const arr = str.split("");
   let newArr = [];
@@ -11,3 +12,25 @@ const reverse = (str) => {
 }
 
 reverse('i am str')
+
+
+
+
+//Other solutions
+function reverse(str){
+  if(!str || typeof str != 'string' || str.length < 2 ) return str;
+  
+  const backwards = [];
+  const totalItems = str.length - 1;
+  for(let i = totalItems; i >= 0; i--){
+    backwards.push(str[i]);
+  }
+  return backwards.join('');
+}
+
+function reverse2(str){
+  //check for valid input
+  return str.split('').reverse().join('');
+}
+
+const reverse3 = str => [...str].reverse().join('');
