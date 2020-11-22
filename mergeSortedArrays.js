abf4941a3f74b60689fe617e22ec18d3e701e157
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-11-22 22:46:21 +0200

    Update mergeSortedArrays.js

--- mergeSortedArrays.js
+++ mergeSortedArrays.js
@@ -1,3 +1,37 @@
+// My solution
 const mergeSortedArrays = (ar1, arr2) => [...ar1, ...arr2].sort((a, b) => a - b);
+
+mergeSortedArrays([0,3,4,31], [3,4,6,30]);
+
+
+//Other solutions
+function mergeSortedArrays(array1, array2){
+  if(array1.length === 0) {
+    return array2;
+  }
+  if(array2.length === 0) {
+    return array1;
+  }
+
+  const mergedArray = [];
+  let array1Item = array1[0];
+  let array2Item = array2[0];
+  let i = 1;
+  let j = 1;
+  
+  while (array1Item || array2Item){
+   if(array2Item === undefined || array1Item < array2Item){
+     mergedArray.push(array1Item);
+     array1Item = array1[i];
+     i++;
+   }   
+   else {
+     mergedArray.push(array2Item);
+     array2Item = array2[j];
+     j++;
+   }
+  }
+  return mergedArray;
+}
 
 mergeSortedArrays([0,3,4,31], [3,4,6,30]);
