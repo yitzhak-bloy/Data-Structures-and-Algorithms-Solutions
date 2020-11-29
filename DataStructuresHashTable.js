e81eb4d0d1b5db9fa60c7b7b059931a2cdd0ddbd
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-11-29 16:27:36 +0200

    Update DataStructuresHashTable.js

--- DataStructuresHashTable.js
+++ DataStructuresHashTable.js
@@ -1,21 +1,9 @@
+// My solution
 class HashTable {
   constructor(size){
     this.data = new Array(size);
   }
 
-set (kay, value) {
-  this.data.fill([kay, value], this._hash(kay), this._hash(kay) + 1)
-}
-
-get (kay) {
-  for (let i = 0; i < this.data.length; i++) {
-    if (this.data[i]) {
-      if (this.data[i][0] === kay) {
-        return this.data[i][1]
-      }
-    }
-  }
-}
 
   _hash(key) {
     let hash = 0;
@@ -23,6 +11,65 @@ get (kay) {
         hash = (hash + key.charCodeAt(i) * i) % this.data.length
     }
     return hash;
+  }
+  
+   set (kay, value) {
+    this.data.fill([kay, value], this._hash(kay), this._hash(kay) + 1)
+  }
+
+  get (kay) {
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i]) {
+        if (this.data[i][0] === kay) {
+          return this.data[i][1]
+        }
+      }
+    }
+  }
+}
+
+const myHashTable = new HashTable(50);
+myHashTable.set('grapes', 10000)
+myHashTable.get('grapes')
+myHashTable.set('apples', 9)
+myHashTable.get('apples')
+
+
+// another solution
+class HashTable {
+  constructor(size){
+    this.data = new Array(size);
+    // this.data = [];
+  }
+
+  _hash(key) {
+    let hash = 0;
+    for (let i =0; i < key.length; i++){
+        hash = (hash + key.charCodeAt(i) * i) % this.data.length
+    }
+    return hash;
+  }
+
+  set(key, value) {
+    let address = this._hash(key);
+    if (!this.data[address]) {
+      this.data[address] = [];
+    }
+    this.data[address].push([key, value]);
+    return this.data;
+  }
+
+  get(key){
+    const address = this._hash(key);
+    const currentBucket = this.data[address]
+    if (currentBucket) {
+      for(let i = 0; i < currentBucket.length; i++){
+        if(currentBucket[i][0] === key) {
+          return currentBucket[i][1]
+        }
+      }
+    }
+    return undefined;
   }
 }
 
