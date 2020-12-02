89775c7597fd88e602fe183bdd6015cfc48ab828
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-12-02 14:41:24 +0200

    Update firstRecurringCharacter.js

--- Data Structures/HashTable/firstRecurringCharacter.js
+++ Data Structures/HashTable/firstRecurringCharacter.js
@@ -8,6 +8,7 @@
 //Given an array = [2,3,4,5]:
 //It should return undefined
 
+
 // My solution 
 const firstRecurringCharacter = (input) => { 
   for (i = 0; i < input.length; i++) {
@@ -20,3 +21,19 @@ const firstRecurringCharacter = (input) => {
 }
 
 firstRecurringCharacter([2,5,1,2,3,5,1,2,4])
+
+
+// another solution, With an efficiency of O(N) instead of O(N^N).
+function firstRecurringCharacter2(input) {
+  let map = {};
+  for (let i = 0; i < input.length; i++) {
+    if (map[input[i]] !== undefined) {
+      return input[i]
+    } else {
+      map[input[i]] = i;
+    }
+  }
+  return undefined
+}
+
+firstRecurringCharacter2([1,5,5,1,3,4,6])
